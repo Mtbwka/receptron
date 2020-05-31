215cdb937b224ce960cd884acc43d20fc78dd3bd
Viktor Kravchuk <viktor.kravchuk@buffl.be>
2020-05-31 17:54:53 +0300

    Dictionary added

--- src/helpers/chunkArray.js
+++ src/helpers/chunkArray.js
@@ -1,17 +1,15 @@
 /**
  * Returns an array with arrays of the given size.
  *
- * @param arr {Array} array to split
+ * @param array {Array} array to split
  * @param chunkSize {Integer} Size of every group
  */
 
 export default (array, chunkSize) => {
   const results = [];
 
-  const filteredArray = array.map((e) => e.title);
-
-  while (filteredArray.length) {
-    results.push(filteredArray.splice(0, chunkSize));
+  while (array.length) {
+    results.push(array.splice(0, chunkSize));
   }
 
   return results;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import TelegramBot from 'node-telegram-bot-api';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import categories from '../db/categories';
+import products from '../db/products';
 import chunkArray from './helpers/chunkArray';
 
 const url = 'https://mnevkusno.ru';
@@ -18,11 +19,48 @@ const fetchData = async () => {
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const providedPoducts = [];
 
 bot.on('message', async (msg) => {
-  bot.sendMessage(msg.chat.id, 'Выберите категорию из списка', {
-    reply_markup: {
-      keyboard: chunkArray(categories, 3),
-    },
-  });
+  const product = products.find((p) => p.title === msg.text);
+
+  if (product) {
+    providedPoducts.push(product);
+    bot.sendMessage(msg.chat.id, `Добавлено ${product.title}`);
+  } else {
+    debugger;
+
+    bot.sendMessage(msg.chat.id, 'Выберите категорию из списка', {
+      reply_markup: {
+        inline_keyboard: chunkArray(
+          categories.map((c) => ({ text: c.title, callback_data: c.id })),
+          2
+        ),
+      },
+    });
+
+    bot.on('callback_query', (query) => {
+      const { message: { chat, message_id, text } = {} } = query;
+
+      const filteredProducts = chunkArray(
+        products
+          .filter((p) => p.category === Number(query.data))
+          .map((p) => p.title),
+        3
+      );
+      if (filteredProducts.length > 0) {
+        bot.sendMessage(chat.id, 'Выберите продукт', {
+          reply_markup: {
+            keyboard: filteredProducts,
+          },
+        });
+      } else {
+        bot.sendMessage(chat.id, 'Not found!');
+      }
+    });
+  }
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, `${providedPoducts.map((p) => p.title)} `);
 });
